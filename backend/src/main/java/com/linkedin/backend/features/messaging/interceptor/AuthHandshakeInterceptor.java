be2cba9c047e6fd0afa68bcf960742f736b7cdb8
yousoumar <oumaiyr@gmail.com>
2024-12-04 09:26:52 +0100

    wip

--- backend/src/main/java/com/linkedin/backend/features/messaging/interceptor/AuthHandshakeInterceptor.java
+++ backend/src/main/java/com/linkedin/backend/features/messaging/interceptor/AuthHandshakeInterceptor.java
@@ -1,8 +1,6 @@
 package com.linkedin.backend.features.messaging.interceptor;
 
 import com.linkedin.backend.features.authentication.utils.JsonWebToken;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -13,7 +11,6 @@ import java.util.Map;
 
 @Component
 public class AuthHandshakeInterceptor implements HandshakeInterceptor {
-    private static final Logger log = LoggerFactory.getLogger(AuthHandshakeInterceptor.class);
     private final JsonWebToken jsonWebToken;
 
     public AuthHandshakeInterceptor(JsonWebToken jsonWebToken) {
