30d1c2611390b7ad6e3d7c36d2a99cb20de06809
yousoumar <oumaiyr@gmail.com>
2024-12-18 07:04:22 +0100

    wip

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -29,7 +29,6 @@ export function Header() {
         .length,
     0
   );
-  const [loadingConversations, setLoadingConversations] = useState(false);
   useEffect(() => {
     const handleResize = () => {
       setShowNavigationMenu(window.innerWidth > 1080);
@@ -39,13 +38,9 @@ export function Header() {
   }, []);
 
   useEffect(() => {
-    setLoadingConversations(true);
     request<Conversation[]>({
       endpoint: "/api/v1/messaging/conversations",
-      onSuccess: (data) => {
-        setConversations(data);
-        setLoadingConversations(false);
-      },
+      onSuccess: (data) => setConversations(data),
       onFailure: (error) => console.log(error),
     });
   }, [location.pathname]);
@@ -200,9 +195,7 @@ export function Header() {
                     <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
                   </svg>
                   <div>
-                    {nonReadMessagesCount > 0 &&
-                    !location.pathname.includes("messaging") &&
-                    !loadingConversations ? (
+                    {nonReadMessagesCount > 0 && !location.pathname.includes("messaging") ? (
                       <span className={classes.badge}>{nonReadMessagesCount}</span>
                     ) : null}
                     <span>Messaging</span>
